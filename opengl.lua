bdb589f57299667d9c6f6025741b26690be014fc
Andrew McWatters <Andrew McWatters>
2016-12-21 19:54:09 -0700

    Revert "Update opengl.lua"
    This reverts commit 711eb8018bacc2be8550e430ec28d1fdeb1d3e21.

--- opengl.lua
+++ opengl.lua
@@ -7,12 +7,4 @@
 local ffi = require( "ffi" )
 io.input( "gl3.h" )
 ffi.cdef( io.read( "*all" ) )
-
-local name = "opengl32"
-if ( jit.os == "OSX" ) then
-	name = "OpenGL.framework/OpenGL"
-else
-	name = "GL"
-end
-
-return ffi.load( name )
+return ffi.load( "OpenGL.framework/OpenGL" )
